c04f917988015205311b123807ebd5c05d7a16b2
TheYisus07 <78822327+TheYisus07@users.noreply.github.com>
2021-05-11 18:21:35 -0500

    ProyectoCostruccion v1

---
+++ src/businesslogic/IEventDAO.java
@@ -0,0 +1,17 @@
+package businesslogic;
+import java.util.List;
+import domain.Event;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Antonio de Jesús Dominguez García
+ */
+public interface IEventDAO {
+    public Event consultEvent(String tittle);
+    public Event scheduleEvent(Event event);
+    public void deleteEvent(Event event);
+    public Event modifyEvent(Event newEvent, String tittle);
+    public ArrayList<Event> searchEvent(String tittle);
+    public ArrayList<Event> checkEventHistory();
+}
